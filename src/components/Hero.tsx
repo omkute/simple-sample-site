e18d1d413f7c139bf02767dd863c0ba97b2c92fc
omkute <58985673+omkute@users.noreply.github.com>
2025-03-07 18:47:30 +0000

    Final MVP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,13 +23,12 @@ const Hero = () => {
             >
               Register Company !
             </Link>
-            <a 
-               href="https://forms.gle/ZsREE7ncc5dCS3fY8" 
-               target="_blank" 
+            <Link 
+              to='/market' 
               className="btn bg-transparent text-white border border-white hover:bg-white hover:bg-opacity-10 px-6 py-3 rounded-full font-medium"
             >
               Start Trading
-            </a>
+            </Link>
           </div>
         </div>
       </div>
